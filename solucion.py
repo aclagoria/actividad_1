720637afad8974ba86688fc51c38008eb4d50669
Carolina Lagoria <lagoriacarolina@gmail.com>
2025-09-02 01:10:16 -0300

    Fuente discreta sin memoria

--- solucion.py
+++ solucion.py
@@ -1,1 +1,21 @@
 
+# Fuente Discreta sin Memoria (Discrete Memoryless Source,DMS)
+
+from random import choices
+
+def fuente(tabla: dict): 
+    simbolos = list(tabla.keys())
+    frecs = list(tabla.values())
+
+    def f(cantidad):
+        return ''.join(choices(simbolos, weights=frecs, k=cantidad))
+    return f
+
+# Análisis de frecuencias 
+
+
+# Arbol de Huffman
+
+# Tabla de códigos
+
+# Codificación y Decodificación
