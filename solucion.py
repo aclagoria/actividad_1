447e638e43a1284a85a25d1e3f0ef0551bd56cf0
Carolina Lagoria <lagoriacarolina@gmail.com>
2025-09-07 23:41:41 -0300

    tabla de codigi

--- solucion.py
+++ solucion.py
@@ -33,8 +33,24 @@ def huffman(frecs):  # funcion cuyo argumento es el diccionario frecs
         y=[[a[0],b[0]],a[1]+b[1]] # crea un nuevo nodo combinandeo a y b
         arboles.append(y)   # agrega y al final de la lista arboles
         arboles.sort(key=lambda x:x[1]) # ordena los nodos según su frecuencia (menor a mayor)
-    return arboles[0]
+    return arboles[0][0] # devuelve la lista anidada con las hojas  
 
 # Tabla de códigos
+def tabla_codigo(arbol, prefijo = ''): # la funcion a partir de una lista arbol entregará un diccionario, se inicializa con un prefijo vacío y en cada llamada recursiva se aumenta un 0 si se va a la izquierda y un 1 si se va a la derecha
+    n= len(arbol) # mide la cantidad de elemtos en el nodo
+
+    match n:
+        case 2: # tiene dos ramas, se hacen llamadas recursivas
+            iz=tabla_codigo(arbol[0],prefijo+'0') # se agrega 0 al prefijo para la rama izquierda
+            de=tabla_codigo(arbol[1],prefijo+'1') # se agrega 1 al prefijo para la rama derecha
+            return iz|de # | une dos diccionaros en uno nuevo, une los diccionarios generados en ambas ramas
+        case 1: # es una hoja
+            return{arbol[0]:prefijo} # devuelve un diccionario el símbolo es la clave y el prefijo su valor
+        case 0: # nodo vacio
+            return dict() # devuelve un diccionario vacío
+        case _:
+            raise ValueError("Arbol mal formado")
+
+
 
 # Codificación y Decodificación
